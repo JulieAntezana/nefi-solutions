530e1e166a6623f8f279f6a03a2a37a7ad4a4a48
Julie Antezana <jantezana.edu@gmail.com>
2023-11-25 14:22:14 -0600

    troubleshoot render connection

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,7 +5,7 @@ pipeline {
         stage('Clone') {
             steps {
                 echo 'Cloning the repository...'
-                git 'https://github.com/Nefi-Solutions/Floral.git' // Replace with your GitHub repo URL
+                git 'https://github.com/JulieAntezana/nefi-solutions.git' // Replace with your GitHub repo URL
             }
         }
 
@@ -19,7 +19,7 @@ pipeline {
         stage('Build') {
             steps {
                 echo 'Building the application...'
-                sh 'npm run build'
+                sh 'npm run build && npm server.js'
             }
         }
 
--- server.js
+++ server.js
@@ -24,20 +24,6 @@ app.use(cookieParser());
 
 app.use(logger('dev')); // Tell express to use the Morgan logger
 
-// Add support for CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-  );
-  next();
-});
-
 // Add support for CORS options while in production
 var corsOptions;
 if (process.env.NODE_ENV === 'production') {
@@ -62,7 +48,6 @@ app.use(express.static(path.join(__dirname, 'dist/floral')));
 app.use('/', index);
 
 // CODE TO MAP URL'S TO ROUTING FILES
-app.use("/", index);
 app.use("/flowers", flowerRoutes);
 
 // Tell express to map all other non-defined routes back to the index page
@@ -86,3 +71,93 @@ const server = http.createServer(app);
 server.listen(port, function() {
   console.log('API running on localhost: ' + port)
 });
+
+
+// // Get dependencies
+// var express = require('express');
+// var path = require('path');
+// var http = require('http');
+// var bodyParser = require('body-parser');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// var mongoose = require('mongoose');
+// require('dotenv').config();
+// const cors = require('cors');
+
+// // Routes - import the routing file to handle the default (index) route
+// const index = require('./server/routes/app');
+// const flowerRoutes = require('./server/routes/flowers');
+
+// var app = express(); // create an instance of express
+
+// // Tell express to use the following parsers for POST data
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//   extended: false
+// }));
+// app.use(cookieParser());
+
+// app.use(logger('dev')); // Tell express to use the Morgan logger
+
+// // Add support for CORS
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+//   );
+//   next();
+// });
+
+// // Add support for CORS options while in production
+// var corsOptions;
+// if (process.env.NODE_ENV === 'production') {
+//   corsOptions = {
+//     origin: 'https://nefi-solutions-floral.onrender.com',
+//     optionsSuccessStatus: 200 // For legacy browser support
+//   };
+// } else {
+//   corsOptions = {
+//     origin: '*',
+//     optionsSuccessStatus: 200 // For legacy browser support
+//   };
+// }
+
+// app.use(cors(corsOptions));
+
+// // Tell express to use the specified directory as the
+// // root directory for your web site
+// app.use(express.static(path.join(__dirname, 'dist/floral')));
+
+// // Tell express to map the default route ('/') to the index route
+// app.use('/', index);
+
+// // CODE TO MAP URL'S TO ROUTING FILES
+// app.use("/", index);
+// app.use("/flowers", flowerRoutes);
+
+// // Tell express to map all other non-defined routes back to the index page
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist/floral/index.html'));
+// });
+
+// mongoose
+//   .connect(process.env.URI, { useNewUrlParser: true })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.log("Connection failed: " + err));
+  
+// // Define the port address and tell express to use this port
+// const port = process.env.PORT || '3000';
+// app.set('port', port);
+
+// // Create HTTP server.
+// const server = http.createServer(app);
+
+// // Tell the server to start listening on the provided port
+// server.listen(port, function() {
+//   console.log('API running on localhost: ' + port)
+// });
