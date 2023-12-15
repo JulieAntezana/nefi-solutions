5155a5d54ad48cf9be06c13c54ee5b2c20139460
Julie Antezana <jantezana.edu@gmail.com>
2023-12-15 17:20:36 -0600

    added firebase

---
+++ environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    URI: 'mongodb+srv://jantezana:S5CGGkcTiPa1QEFx@cluster0.gxn4qk2.mongodb.net/'
+}
---
+++ environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    URI: 'mongodb+srv://jantezana:S5CGGkcTiPa1QEFx@cluster0.gxn4qk2.mongodb.net/'
+}
