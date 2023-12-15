dbe550c204e68dc1b7ae39690bcbddb270128625
Julie Antezana <jantezana.edu@gmail.com>
2023-12-15 00:48:37 -0600

    change it back

--- protractor.conf.js
+++ protractor.conf.js
@@ -8,7 +8,7 @@ exports.config = {
         args: ['--headless', '--disable-gpu', '--window-size=800,600'],
       },
     },
-    // chromeDriverVersion: '120.0.6099.71',
+    chromeDriverVersion: '120.0.6099.71',
     baseUrl: 'http://34.174.97.158:4200/', // Update with your application URL
     jasmineNodeOpts: {
       showColors: true,
